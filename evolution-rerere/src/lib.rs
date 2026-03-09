078cbe7757140d25f67cb8af1c671b0ec7442a33
Ondřej Hošek <ondra.hosek@gmail.com>
2026-03-09 18:44:50 +0100

    export the necessary functions

--- evolution-rerere/src/lib.rs
+++ evolution-rerere/src/lib.rs
@@ -7,9 +7,11 @@ use evolution_glue::GTypeModule;
 use crate::ext::ReReReComposerExtension;
 
 
+#[unsafe(no_mangle)]
 pub extern "C" fn e_module_load(type_module: *mut GTypeModule) {
     ReReReComposerExtension::register_type(type_module);
 }
 
+#[unsafe(no_mangle)]
 pub extern "C" fn e_module_unload(_type_module: *mut GTypeModule) {
 }
